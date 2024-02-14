23d1d19c0a127ea2a2c2b675fe0e42af7d1a2c00
Jojo42729 <jlanguedocgit@gmail.com>
2024-02-14 15:50:41 -0500

    added login and logout paths to urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,21 +5,14 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-
-    path(route='about', view=views.about, name='about'),
-
-    path(route='contact', view=views.contact, name='contact'),
+    path(route='about', view=views.about, name='about'), #path for about page
+    path(route='contact', view=views.contact, name='contact'), #path for contact page
 
     # path for registration
 
-    # path for login
-
-    # path for logout
-
-    path(route='', view=views.get_dealerships, name='index'),
+    path(route='login', views.login_request, name='login'), #path for login requests
+    path(route='logout', views.logout_request, name='logout'), #path for logout requests
+    path(route='', view=views.get_dealerships, name='index'), #path for index page
 
     # path for dealer reviews view
 
